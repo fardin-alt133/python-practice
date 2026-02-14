ad0e02aec2039bd4aa7506da8e89b1fe87c0b043
Fardin <fariyadfardin@gmail.com>
2026-02-14 11:45:22 +0600

    Add files via upload

---
+++ Simple Interest Calculator.py
@@ -0,0 +1,5 @@
+x=float(input("Enter the principal amount: "))
+y=float(input("Enter the rate of interest: "))
+z=float(input("Enter the time in years: "))
+simple_interest=(x*y*z)/100
+print("The simple interest is: ",simple_interest)
